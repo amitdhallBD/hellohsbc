1d1944dda7719fe826ba05a91723f440fca23838
Amit Dhall <amit.dhall@appworks.co.in>
2018-10-30 16:34:03 +0530

    initial commit

---
+++ sayHello/src/main/java/com/ad/devops/sayHello/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.ad.devops.sayHello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ sayHello/src/test/java/com/ad/devops/sayHello/HelloTest.java
@@ -0,0 +1,15 @@
+package com.ad.devops.sayHello;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
